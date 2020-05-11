002a7aafcbffad52f5cc2a5eb50eb24216cad2d5
Marian Theisen <marian@cice-online.net>
2020-05-11 09:59:16 +0200

    update docs

--- lib/operators/all-true.ts
+++ lib/operators/all-true.ts
@@ -5,6 +5,12 @@
 import {combineLatest, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
+/**
+ * Combines all input boolean observables and emits true if all events are true,
+ * false otherwise.
+ *
+ * @param inputs
+ */
 export function allTrue(...inputs: Array<Observable<boolean>>): Observable<boolean> {
   return combineLatest(
     ...inputs,
