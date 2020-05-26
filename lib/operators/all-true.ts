e50224a4e20423520b5edd3e57fc5796f5cecfa5
Marian Theisen <marian@cice-online.net>
2020-05-26 14:01:16 +0200

    add gh-pages deploy action

--- lib/operators/all-true.ts
+++ lib/operators/all-true.ts
@@ -12,9 +12,7 @@ import {map} from 'rxjs/operators';
  * @param inputs
  */
 export function allTrue(...inputs: Array<Observable<boolean>>): Observable<boolean> {
-  return combineLatest(
-    ...inputs,
-  )
+  return combineLatest(inputs)
     .pipe(
       map((values: boolean[]) => {
         for (let val of values) {
